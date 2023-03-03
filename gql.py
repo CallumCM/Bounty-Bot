828bd3c26e39e416b31b5d499e767d1c9fff1bbc
Callum <51388022+CallumCM@users.noreply.github.com>
2023-03-03 04:47:02 +0000

    Add an exception in gql.py just in case Replit doesn't return valid bounties

--- gql.py
+++ gql.py
@@ -16,20 +16,27 @@ class Gql():
                             "Cookie": "connect.sid=" + self.sid
                         }).json()
         except:
-            return {
-                "id": -1,
-                "title": "undefined",
-                "descriptionPreview": "undefined",
-                "cycles": -1,
-                "deadline": "0000-00-06T00:00:00.000Z",
-                "slug": "undefined",
-                "timeCreated": "0000-00-06T00:00:00.000Z",
-                "applicationCount": -1,
-                "user": {
-                    "url": "/@undefined"
-                },
-                "__typename": "Bounty",
-                "url": "https://replit.com/bounties/@undefined/undefined",
-                "timestamp": "<t:0000000000:R>",
-                "dollars": "$-1.00"
-            }
+            return [{
+                "data": {
+                    "bountySearch": {
+                        "items": {
+                            "id": -1,
+                            "title": "undefined",
+                            "descriptionPreview": "undefined",
+                            "cycles": -1,
+                            "deadline": "0000-00-06T00:00:00.000Z",
+                            "slug": "undefined",
+                            "timeCreated": "0000-00-06T00:00:00.000Z",
+                            "applicationCount": -1,
+                            "user": {
+                                "url": "/@undefined"
+                            },
+                            "__typename": "Bounty",
+                            "url":
+                            "https://replit.com/bounties/@undefined/undefined",
+                            "timestamp": "<t:0000000000:R>",
+                            "dollars": "$-1.00"
+                        }
+                    }
+                }
+            }]
